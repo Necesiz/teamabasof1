442a63fb717d9a09b8d1602c8b68082fd42d7a6d
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-06-11 00:09:49 +0400

    update qaqa.py

--- qaqa.py
+++ qaqa.py
@@ -1,52 +1,40 @@
-python
-import requests
-import telegram
-from telegram.ext import Updater, CommandHandler
+import os
+import youtube_dl
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Telegram bot token
-TOKEN = "6290317562:AAHQq06eN1EHUYqVbUWR0k1eSHSCY4AZo-8"
+# indirilecek video için fonksiyonlar
+def download_video(url):
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        video_url = info_dict.get("url", None)
+        video_id = info_dict.get("id", None)
+        video_title = info_dict.get('title', None)
+        ydl.download([url])
+        return video_title + '.mp3'
 
-# Function to handle the /start command
+# Telegram botu için fonksiyonlar
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Merhaba! Bana bir şehir adı söyleyin, size hava durumunu söyleyeyim.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Merhaba, ben müzik botu. İstediğin şarkının YouTube linkini gönder!")
+    
+def handle_message(update, context):
+    message_text = update.message.text
+    chat_id = update.message.chat_id
+    if "https://www.youtube.com/" in message_text:
+        mp3_file = download_video(message_text)
+        context.bot.send_audio(chat_id=chat_id, audio=open(mp3_file, 'rb'), title=mp3_file)
+        os.remove(mp3_file)
+    else:
+        context.bot.send_message(chat_id=chat_id, text="Lütfen sadece YouTube linki gönderin!")
 
-# Function to handle the weather command
-def weather(update, context):
-    # Get the city name from the user input
-    city_name = " ".join(context.args)
+def main():
+    updater = Updater(token="6290317562:AAHQq06eN1EHUYqVbUWR0k1eSHSCY4AZo-8", use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    message_handler = MessageHandler(Filters.text & ~Filters.command, handle_message)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(message_handler)
+    updater.start_polling()
 
-    # Generate the URL for the OpenWeatherMap API
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=your_api_key&lang=tr&units=metric"
-
-    # Request the weather information from the API
-    response = requests.get(url)
-
-    # Parse the JSON response
-    weather_data = response.json()
-
-    # Extract the weather information
-    city = weather_data["name"]
-    description = weather_data["weather"][0]["description"]
-    temp = weather_data["main"]["temp"]
-    feels_like = weather_data["main"]["feels_like"]
-    humidity = weather_data["main"]["humidity"]
-
-    # Compose the response message
-    message = f"Hava durumu: {city}\nDurum: {description}\nSıcaklık: {temp:.1f} °C\nHissedilen sıcaklık: {feels_like:.1f} °C\nNem: {humidity}%"
-
-    # Send the response message to the user
-    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-
-# Setup the Telegram bot
-bot = telegram.Bot(token=TOKEN)
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-# Add the command handlers
-start_handler = CommandHandler("start", start)
-weather_handler = CommandHandler("weather", weather)
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(weather_handler)
-
-# Start the bot
-updater.start_polling()
+if name == 'main':
+    main()
