dc8146c3535f68e9f67f35284a45a945aa268060
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-06-11 00:59:27 +0400

    update qaqa.py

--- qaqa.py
+++ qaqa.py
@@ -1,34 +1,22 @@
 import telebot
 from pytube import YouTube
 
-TOKEN = '6112298959:AAFsCNm4qJ-r9o6GHZswao7cq3wpL9a9ruM'
-bot = telebot.TeleBot(TOKEN)
+# Telegram botunuzun API anahtarını buraya ekleyin
+API_KEY = '6112298959:AAFsCNm4qJ-r9o6GHZswao7cq3wpL9a9ruM'
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    welcome_message = "Merhaba! YouTube videolarını veya shorts'ları indirmek için bana bir YouTube linki gönderin."
-    bot.reply_to(message, welcome_message)
+bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(func=lambda message: True)
-def download_video(message):
-    try:
-        video_url = message.text
-        video = YouTube(video_url)
-        video_title = video.title
-
-        # İndirme işlemi
-        stream = video.streams.get_highest_resolution()
-        stream.download(filename='video')
-
-        # İndirilen videoyu gönderme
-        with open('video.mp4', 'rb') as video_file:
-            bot.send_video(message.chat.id, video_file)
-
-        # İndirme tamamlandı mesajı
-        download_complete_message = f"{video_title} başarıyla indirildi."
-        bot.reply_to(message, download_complete_message)
-    except Exception as e:
-        error_message = f"Bir hata oluştu: {str(e)}"
-        bot.reply_to(message, error_message)
+def handle_message(message):
+    text = message.text
+    if text.startswith('https://www.youtube.com/') or text.startswith('https://youtu.be/'):
+        try:
+            yt = YouTube(text)
+            video = yt.streams.get_highest_resolution()
+            video.download()
+            bot.send_message(message.chat.id, 'Video indirildi.')
+        except Exception as e:
+            bot.send_message(message.chat.id, 'Video indirilirken bir hata oluştu.')
+            print(str(e))
 
 bot.polling()
