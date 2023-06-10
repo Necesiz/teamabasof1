179ac7f0163378dde4cba1f92c6a5b7ba26c562e
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-06-11 01:38:34 +0400

    update qaqa.py

--- qaqa.py
+++ qaqa.py
@@ -1,22 +1,45 @@
 import telebot
 
-# Telegram Bot Tokenini buraya ekleyin
-TOKEN = '6112298959:AAFsCNm4qJ-r9o6GHZswao7cq3wpL9a9ruM'
+bot = telebot.TeleBot("6112298959:AAFsCNm4qJ-r9o6GHZswao7cq3wpL9a9ruM")  # Botunuzun tokenını buraya yerleştirin
 
-# Telebot nesnesini oluşturun
-bot = telebot.TeleBot(TOKEN)
+blocklist = []  # Blocklist için boş bir liste
 
-@bot.message_handler(func=lambda message: message.chat.type == 'supergroup' and message.new_chat_members)
-def on_chat_join_request(message):
-    for user in message.new_chat_members:
-        # Kullanıcının grup isteğini kabul etmek için bir işlem yapabilirsiniz
-        # Örneğin, kullanıcıya hoş geldin mesajı gönderebiliriz
-        bot.send_message(user.id, 'Gruba hoş geldiniz!')
+@bot.message_handler(func=lambda message: True)
+def check_message(message):
+    user_id = message.from_user.id
+    text = message.text.lower()
+    
+    for word in blocklist:
+        if word in text:
+            if user_id not in blocklist:
+                blocklist.append(user_id)
+                bot.reply_to(message, "Blocklistteki bir kelimeyi kullandığınız için uyarıldınız.")
+                break
+            elif blocklist.count(user_id) < 3:
+                blocklist.append(user_id)
+                bot.reply_to(message, "Blocklistteki bir kelimeyi tekrar kullandığınız için uyarıldınız. Bu 3. uyarınız, bir sonraki uyarıda banlanacaksınız.")
+                break
+            else:
+                bot.kick_chat_member(message.chat.id, user_id)
+                bot.reply_to(message, "Blocklistteki bir kelimeyi 3 kez kullandığınız için banlandınız.")
+                break
 
-@bot.message_handler(func=lambda message: message.chat.type == 'private')
-def on_private_message(message):
-    # Kullanıcının özel mesajına istediğiniz mesajı gönderebilirsiniz
-    bot.send_message(message.chat.id, 'Özel mesajınıza gönderilen bir mesaj!')
+@bot.message_handler(commands=['add'])
+def add_word_to_blocklist(message):
+    word = message.text.split('/add ', 1)[1].lower()
+    if word not in blocklist:
+        blocklist.append(word)
+        bot.reply_to(message, f"{word} blockliste eklendi.")
+    else:
+        bot.reply_to(message, f"{word} zaten blocklistte yer alıyor.")
 
-# Botu çalıştırın
+@bot.message_handler(commands=['remove'])
+def remove_word_from_blocklist(message):
+    word = message.text.split('/remove ', 1)[1].lower()
+    if word in blocklist:
+        blocklist.remove(word)
+        bot.reply_to(message, f"{word} blocklistten kaldırıldı.")
+    else:
+        bot.reply_to(message, f"{word} blocklistte bulunamadı.")
+
 bot.polling()
